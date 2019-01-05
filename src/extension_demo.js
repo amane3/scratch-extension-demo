b5afad6645638970c67933a94e620f59a63de23e
amane <furuya0427@gmail.com>
2019-01-05 15:25:48 +0530

    commit

--- src/extension_demo.js
+++ src/extension_demo.js
@@ -2,8 +2,57 @@
 
 ext._shutdown = function() {};
 ext._getStatus = function() {
-    return {status: 2,msg: "Ready"};
-};
+   if(!device) return {status: 1, msg: 'Device not connected'};
+   return {status: 2, msg: 'Device connected'};
+}
+
+var potentialDevices = [];
+    ext._deviceConnected = function(dev) {
+        potentialDevices.push(dev);
+
+        if (!device) {
+            tryNextDevice();
+        }
+    }
+
+    var poller = null;
+    var watchdog = null;
+    function tryNextDevice() {
+        // If potentialDevices is empty, device will be undefined.
+        // That will get us back here next time a device is connected.
+        device = potentialDevices.shift();
+        if (!device) return;
+
+        device.open({ stopBits: 0, bitRate: 38400, ctsFlowControl: 0 });
+        device.set_receive_handler(function(data) {
+            //console.log('Received: ' + data.byteLength);
+            if(!rawData || rawData.byteLength == 18) rawData = new Uint8Array(data);
+            else rawData = appendBuffer(rawData, data);
+
+            if(rawData.byteLength >= 18) {
+                //console.log(rawData);
+                processData();
+                //device.send(pingCmd.buffer);
+            }
+        });
+
+        // Tell the PicoBoard to send a input data every 50ms
+        var pingCmd = new Uint8Array(1);
+        pingCmd[0] = 1;
+        poller = setInterval(function() {
+            device.send(pingCmd.buffer);
+        }, 50);
+        watchdog = setTimeout(function() {
+            // This device didn't get good data in time, so give up on it. Clean up and then move on.
+            // If we get good data then we'll terminate this watchdog.
+            clearInterval(poller);
+            poller = null;
+            device.set_receive_handler(null);
+            device.close();
+            device = null;
+            tryNextDevice();
+        }, 250);
+    };
 
 ext.log_test = function(str) {
 // do something
@@ -36,8 +85,7 @@ var descriptor = {
    menus:menus,
 };
 
+var serial_info = {type: 'serial'};
+ScratchExtensions.register('Hifive1', descriptor, ext, serial_info);
 
-
-
-ScratchExtensions.register('Log Extension', descriptor, ext);
 })({});
