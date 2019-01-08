8b4718d577062bee93dbcc70670736af22536f7e
amane <furuya0427@gmail.com>
2019-01-08 22:13:00 +0530

    commit

--- src/extension_demo.js
+++ src/extension_demo.js
@@ -15,14 +15,14 @@ function ascii(a){
 //sending buffer to board
 function analogWrite(msg){
     console.log(msg);
-    var buf = new Uint8Array([0x96,0x96,0x15]);
+    //var buf = new Uint8Array([0x96,0x96,0x15]);
     //buf[0] = ascii(msg);
     //buf[1] = ascii("\n");
     //console.log(buf[0]);
     //console.log(buf[1]);
     //device.send(buf.buffer);
-    var buffer = Buffer.from(buf,'base64');
-    device.send_raw(buffer);
+    var buf = Buffer.from(ascii(msg+"\n"),'base64');
+    device.send_raw(buf);
 }
 
 ext.log_test = function(str) {
